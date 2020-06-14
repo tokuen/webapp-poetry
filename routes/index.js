a2dcbbbd4937740f0266a7240f24ec19c94cb22e
toku <guoyongyue1996@163.com>
2020-06-14 17:59:05 +0800

    add
    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request.post({url:'http://localhost:8080/poetry/poetry/getRandomPoetry', form: {"name" : "john"}}, 
+    function (err, httpResponse, body) {
+        if (err) {
+          res.render('error');
+          return console.error('upload failed:', err);
+        }else{
+        	data = JSON.parse(body)
+          console.error('upload success:', data)
+          console.error('upload success return_message:', data.return_message)
+          res.render('index', 
+            {
+              data : data.data
+            }
+          );
+        }
+        
+    }
+  );
 });
-
 module.exports = router;
+
